5e39736a51a2f2eab0ac16088433b15fd5888622
Альгиз <algiz.gm@gmail.com>
2025-10-26 14:10:34 +0000

    we don need this window anymore in this view

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -7,7 +7,6 @@ import { AddExerciseDialog } from '@/components/add-exercise-dialog';
 import { AddHabitDialog } from '@/components/add-habit-dialog';
 import type { Exercise, Habit } from '@/lib/types';
 import { exercises as initialExercises, habits as initialHabits } from '@/lib/data';
-import { HabitList } from '@/components/habit-list';
 import { ManageCategoriesDialog } from '@/components/manage-categories-dialog';
 import { Button } from '@/components/ui/button';
 import { Plus } from 'lucide-react';
@@ -66,31 +65,17 @@ export default function SchedulePage() {
                 />
           </div>
         </div>
-        <div className="grid grid-cols-1 gap-4 lg:grid-cols-10">
-          <div className="lg:col-span-6">
-            <DailySchedule 
-              exercises={exercises} 
-              habits={habits} 
-              onExerciseAdd={handleAddExercise} 
-              onHabitAdd={handleAddHabit}
-              onExerciseUpdate={handleUpdateExercise}
-              onHabitUpdate={handleUpdateHabit}
-              onExerciseDelete={handleDeleteExercise}
-              onHabitDelete={handleDeleteHabit}
-              openManageCategories={() => setIsManageCategoriesOpen(true)}
-              />
-          </div>
-          <div className="lg:col-span-4">
-            <HabitList 
-              habits={habits} 
-              onHabitAdd={handleAddHabit}
-              onHabitUpdate={handleUpdateHabit}
-              onHabitDelete={handleDeleteHabit}
-              setHabits={setHabits} 
-              openManageCategories={() => setIsManageCategoriesOpen(true)}
-              />
-          </div>
-        </div>
+        <DailySchedule 
+          exercises={exercises} 
+          habits={habits} 
+          onExerciseAdd={handleAddExercise} 
+          onHabitAdd={handleAddHabit}
+          onExerciseUpdate={handleUpdateExercise}
+          onHabitUpdate={handleUpdateHabit}
+          onExerciseDelete={handleDeleteExercise}
+          onHabitDelete={handleDeleteHabit}
+          openManageCategories={() => setIsManageCategoriesOpen(true)}
+        />
       </div>
       <ManageCategoriesDialog open={isManageCategoriesOpen} onOpenChange={setIsManageCategoriesOpen} />
     </>
